d0bdc2896d30af2531d5d940281edaf4fba27637
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-07 15:08:25 +0530

    add basic and cors configurations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,26 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
 
 app.get("/test", (req, res) => {
     res.send("Hello this is testing request")
 })
+
+
+// Basic configurations
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+// CORS configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 
 export default app;
