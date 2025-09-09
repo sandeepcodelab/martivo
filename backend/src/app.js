5c739ddd26b0524fcc9dfda46274f650ee475c90
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-09 15:01:08 +0530

    add healthcheck route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,31 @@
 import express from "express";
 import cors from "cors";
 
+// Routers
+import healthcheckRouter from "./routes/healthcheckRoute.js";
 
 const app = express();
 
 app.get("/test", (req, res) => {
-    res.send("Hello this is testing request")
-})
-
+  res.send("Hello this is testing request");
+});
 
 // Basic configurations
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 // CORS configuration
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}))
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+// Routes
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/DB_Config.js";
 
-
-dotenv.config({ 
-        path: "./.env"
-    });
-
+dotenv.config({
+  path: "./.env",
+});
 
 const port = process.env.PORT || 8001;
 
-app.listen(port, () => {
-    console.log(`App is running on port: ${port}, http://localhost:${port}`);
-})
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is running on port: ${port}, http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error: ", err);
+  });
