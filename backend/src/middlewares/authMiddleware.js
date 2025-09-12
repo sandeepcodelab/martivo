f2372b9e6dca1c95d818c8bc1771c0eb5ea48e4d
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-12 16:12:54 +0530

    add authentication middleware

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+import { User } from "../models/userModel.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
+
+  if (!token) {
+    throw new ApiError(401, "Unauthorized access. Please login to continue.");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -forgotPasswordToken -forgotPasswordExpiry -emailVerificationToken -emailVerificationExpiry"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid token. Please login again to continue.");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, "Invalid token. Please login again to continue.");
+  }
+});
