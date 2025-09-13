5032ba5235a59dae0f7a83872b6cb810f9705545
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-13 16:45:17 +0530

    add logout route

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController.js";
+import {
+  registerUser,
+  login,
+  logoutUser,
+} from "../controllers/authController.js";
 import { validate } from "../middlewares/validatorMiddleware.js";
-import { userRegisterValidator } from "../validators/index.js";
+import {
+  userLoginValidator,
+  userRegisterValidator,
+} from "../validators/index.js";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
