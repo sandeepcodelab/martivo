55997c88820b64e03bad58c9aebd065014c5813f
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-17 18:35:58 +0530

    add category model

---
+++ backend/src/models/categoryModel.js
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "parentCategory",
+      default: null,
+    },
+    status: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Category = mongoose.model("Category", categorySchema);
