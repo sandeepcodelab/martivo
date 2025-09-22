bdd311b516dc8e6003f732b1450b2b69566186f0
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-22 22:22:37 +0530

    add config and functionality to upload on cloudinary

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      folder: "martivo",
+      resource_type: "auto",
+    });
+
+    console.log("File uploaded successfully. File src: " + response.url);
+
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
