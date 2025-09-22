f41013c956c321264a278e139ede150bea058ff2
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-22 22:25:21 +0530

    add functionality to upload images

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -2,20 +2,39 @@ import { Product } from "../models/productsModel.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const addProduct = asyncHandler(async (req, res) => {
-  const {
-    title,
-    description,
-    category,
-    productImage,
-    productSubImage,
-    status,
-  } = req.body;
+  const { title, description, category, status } = req.body;
 
-  //   if (!title) {
-  //     throw new ApiError(400, "Title field cannot be empty.", []);
-  //   }
+  const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+  const imagesLocalPath = req.files?.images;
+
+  if (thumbnailLocalPath) {
+    let thumbnail = "";
+
+    try {
+      thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+    } catch (error) {
+      throw new ApiError(500, "Failed to upload thumbnail.", []);
+    }
+  }
+
+  if (imagesLocalPath.length > 0) {
+    let images = [];
+
+    try {
+      await Promise.all(
+        imagesLocalPath.map(async (image) => {
+          const uploadedImg = await uploadOnCloudinary(image.path);
+          images.push(uploadedImg.secure_url);
+        })
+      );
+    } catch (error) {
+      throw new ApiError(500, "Failed to upload thumbnail.", []);
+    }
+    console.log("images => ", images);
+  }
 
   return res.status(201).json(new ApiResponse(201, {}, "created!"));
 });
