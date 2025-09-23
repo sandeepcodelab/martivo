273e8d0de868509739f1160a754ea9a8e50f0888
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-23 17:24:26 +0530

    add get single product functionality

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -97,6 +97,20 @@ const getAllProducts = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { products }, "Products fetched successfully."));
 });
 
+const editProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    throw new ApiError(404, "Product not found.", []);
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { product }, "Product fetched successfully."));
+});
+
 // const addProduct = asyncHandler(async(req, res) => {})
 
-export { addProduct, getAllProducts };
+export { addProduct, getAllProducts, editProduct };
