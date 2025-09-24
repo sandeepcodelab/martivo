bf516f72a049b0d52937e4ad9ed3bedf7f01213e
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-24 16:53:26 +0530

    add product routes

--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
-import { addProduct } from "../controllers/productsController.js";
+import {
+  addProduct,
+  getAllProducts,
+  editProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productsController.js";
 import { validate } from "../middlewares/validatorMiddleware.js";
-import { addProductVlidator } from "../validators/index.js";
+import {
+  addProductVlidator,
+  updateProductVlidator,
+} from "../validators/index.js";
 import { upload } from "../middlewares/multerMiddleware.js";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
+import { roleCheck } from "../middlewares/roleMiddleware.js";
 
 const router = Router();
 
+router.route("/all").get(getAllProducts);
+
+// Secured routes
 router.route("/add").post(
   upload.fields([
     {
@@ -18,7 +32,32 @@ router.route("/add").post(
   ]),
   addProductVlidator(),
   validate,
+  verifyJWT,
+  roleCheck(["admin"]),
   addProduct
 );
+
+router.route("/edit/:id").get(verifyJWT, roleCheck(["admin"]), editProduct);
+
+router.route("/update/:id").patch(
+  upload.fields([
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
+    {
+      name: "images",
+    },
+  ]),
+  updateProductVlidator(),
+  validate,
+  verifyJWT,
+  roleCheck(["admin"]),
+  updateProduct
+);
+
+router
+  .route("/delete/:id")
+  .delete(verifyJWT, roleCheck(["admin"]), deleteProduct);
 
 export default router;
