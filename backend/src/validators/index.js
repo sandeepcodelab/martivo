63ba9788495e6e0e1bf98fb86f1991a85b364d37
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-28 17:27:21 +0530

    added add and update variant validator

--- backend/src/validators/index.js
+++ backend/src/validators/index.js
@@ -6,19 +6,19 @@ const userRegisterValidator = () => {
     body("email")
       .trim()
       .notEmpty()
-      .withMessage("Email field cannot be empty.")
+      .withMessage("Email field can not be empty.")
       .isEmail()
       .withMessage("Please enter a valid email address."),
     body("name")
       .trim()
       .notEmpty()
-      .withMessage("Name field cannot be empty.")
+      .withMessage("Name field can not be empty.")
       .isLength({ min: 3 })
       .withMessage("Name must be at least 3 characters long."),
     body("password")
       .trim()
       .notEmpty()
-      .withMessage("Password field cannot be empty.")
+      .withMessage("Password field can not be empty.")
       .isLength({ min: 8 })
       .withMessage("Password must be at least 8 characters in length."),
   ];
@@ -29,13 +29,13 @@ const userLoginValidator = () => {
     body("email")
       .trim()
       .notEmpty()
-      .withMessage("Email field cannot be empty.")
+      .withMessage("Email field can not be empty.")
       .isEmail()
       .withMessage("Please enter a valid email address."),
     body("password")
       .trim()
       .notEmpty()
-      .withMessage("Password field cannot be empty."),
+      .withMessage("Password field can not be empty."),
   ];
 };
 
@@ -44,7 +44,7 @@ const forgotPasswordValidator = () => {
     body("email")
       .trim()
       .notEmpty()
-      .withMessage("Email field cannot be empty.")
+      .withMessage("Email field can not be empty.")
       .isEmail()
       .withMessage("Please enter a valid email address."),
   ];
@@ -55,7 +55,7 @@ const resetForgotPasswordValidator = () => {
     body("newPassword")
       .trim()
       .notEmpty()
-      .withMessage("New password cannot be empty."),
+      .withMessage("New password can not be empty."),
   ];
 };
 
@@ -64,47 +64,100 @@ const changeCurrentPasswordValidator = () => {
     body("oldPassword")
       .trim()
       .notEmpty()
-      .withMessage("Old password cannot be empty."),
+      .withMessage("Old password can not be empty."),
     body("newPassword")
       .trim()
       .notEmpty()
-      .withMessage("New password cannot be empty."),
+      .withMessage("New password can not be empty."),
   ];
 };
 
 // Category validators
 const categoryValidator = () => {
   return [
-    body("name").trim().notEmpty().withMessage("Name field cannot be empty."),
+    body("name").trim().notEmpty().withMessage("Name field can not be empty."),
   ];
 };
 
 // Product validator
 const addProductVlidator = () => {
   return [
-    body("title").trim().notEmpty().withMessage("Title field cannot be empty."),
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("Title field can not be empty."),
     body("description")
       .trim()
       .notEmpty()
-      .withMessage("Description field cannot be empty."),
+      .withMessage("Description field can not be empty."),
     body("category")
       .trim()
       .notEmpty()
-      .withMessage("Catrgory field cannot be empty."),
+      .withMessage("Catrgory field can not be empty."),
   ];
 };
 
 const updateProductVlidator = () => {
   return [
-    body("title").trim().notEmpty().withMessage("Title field cannot be empty."),
+    body("title")
+      .trim()
+      .notEmpty()
+      .withMessage("Title field can not be empty."),
     body("description")
       .trim()
       .notEmpty()
-      .withMessage("Description field cannot be empty."),
+      .withMessage("Description field can not be empty."),
     body("category")
       .trim()
       .notEmpty()
-      .withMessage("Catrgory field cannot be empty."),
+      .withMessage("Catrgory field can not be empty."),
+  ];
+};
+
+// Product variant validator
+const addVariantValidator = () => {
+  return [
+    body("size").trim().notEmpty().withMessage("Size field can not be empty."),
+    body("color")
+      .trim()
+      .notEmpty()
+      .withMessage("Color field can not be empty."),
+    body("price")
+      .notEmpty()
+      .withMessage("Price field can not be empty.")
+      .isFloat({ gt: 0 })
+      .withMessage(
+        "Price must be a valid number (integer or decimal) greater than 0."
+      ),
+    body("stock")
+      .notEmpty()
+      .withMessage("Stock field can not be empty.")
+      .isInt({ min: 0 })
+      .withMessage("Stock must be a valid non-negative integer."),
+    body("sku").trim().notEmpty().withMessage("Sku field can not be empty."),
+  ];
+};
+
+const updateVariantValidator = () => {
+  return [
+    body("size").trim().notEmpty().withMessage("Size field can not be empty."),
+    body("color")
+      .trim()
+      .notEmpty()
+      .withMessage("Color field can not be empty."),
+    body("price")
+      .notEmpty()
+      .withMessage("Price field can not be empty.")
+      .isFloat({ gt: 0 })
+      .withMessage(
+        "Price must be a valid number (integer or decimal) greater than 0."
+      ),
+    body("stock")
+      .notEmpty()
+      .withMessage("Stock field can not be empty.")
+      .isInt({ min: 0 })
+      .withMessage("Stock must be a valid non-negative integer."),
+    body("sku").trim().notEmpty().withMessage("Sku field can not be empty."),
   ];
 };
 
@@ -120,4 +173,7 @@ export {
   // Product validator
   addProductVlidator,
   updateProductVlidator,
+  // Product variant validator
+  addVariantValidator,
+  updateVariantValidator,
 };
