67a8d36370226136db53c9131ec344180091e0fd
sandeepcodelab <sandeep.codes1@gmail.com>
2025-09-28 17:25:24 +0530

    added variant all routes

---
+++ backend/src/routes/variantRoute.js
@@ -0,0 +1,49 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
+import { roleCheck } from "../middlewares/roleMiddleware.js";
+import {
+  addVariant,
+  getAllVariants,
+  getVariantById,
+  updateVariant,
+  deleteVariant,
+} from "../controllers/variantController.js";
+
+import { validate } from "../middlewares/validatorMiddleware.js";
+import {
+  addVariantValidator,
+  updateVariantValidator,
+} from "../validators/index.js";
+
+const router = Router();
+
+router.route("/:productId/all").get(getAllVariants);
+
+// Secured routes
+router
+  .route("/:productId/add")
+  .post(
+    verifyJWT,
+    roleCheck(["admin"]),
+    addVariantValidator(),
+    validate,
+    addVariant
+  );
+
+router.route("/edit/:id").get(verifyJWT, roleCheck(["admin"]), getVariantById);
+
+router
+  .route("/update/:id")
+  .patch(
+    verifyJWT,
+    roleCheck(["admin"]),
+    updateVariantValidator(),
+    validate,
+    updateVariant
+  );
+
+router
+  .route("/delete/:id")
+  .delete(verifyJWT, roleCheck(["admin"]), deleteVariant);
+
+export default router;
