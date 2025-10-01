8e71571fb2504ef98e8b75519c9e22fd60d7e930
sandeepcodelab <sandeep.codes1@gmail.com>
2025-10-01 14:16:36 +0530

    add frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-gray-900">
+      <p className="text-5xl font-bold text-orange-700">Martivo</p>
+    </div>
+  );
+}
+
+export default App;
