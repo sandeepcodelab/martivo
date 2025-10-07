fb8738fcafff2bf9b31e4bf62a469cdec99b12a3
sandeepcodelab <sandeep.codes1@gmail.com>
2025-10-07 22:02:13 +0530

    add filter as sidebar

---
+++ frontend/src/components/Sidebar/Filter.jsx
@@ -0,0 +1,93 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+import { useSidebar } from "@/components/ui/sidebar";
+import {
+  Home,
+  Inbox,
+  Calendar,
+  Search,
+  Settings,
+  Settings2,
+} from "lucide-react";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export function CustomTrigger() {
+  const { toggleSidebar } = useSidebar();
+
+  return (
+    <button
+      onClick={toggleSidebar}
+      className="bg-white flex gap-2 cursor-pointer mt-2 py-1 px-2 rounded-2xl"
+    >
+      <Settings2 />
+      Filter
+    </button>
+  );
+}
+
+export function Filter() {
+  return (
+    <Sidebar collapsible="offcanvas" className="mt-16">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Filter</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
