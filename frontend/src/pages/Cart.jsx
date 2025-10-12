869560fc4176de1f28cc3298e5835009dc24daf4
sandeepcodelab <sandeep.codes1@gmail.com>
2025-10-12 16:59:22 +0530

    add cart page

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,51 @@
+import Container from "@/components/Container/Container";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Cart() {
+  return (
+    <section>
+      <Container>
+        <div className="flex flex-wrap justify-between bg-red-400">
+          <div className="w-full lg:w-3xl">
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+          <div className="w-full lg:w-md">
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
