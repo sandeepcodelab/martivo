c36254a410fcd6296d6e7b42eccc0fa26e4a8fd5
sandeepcodelab <sandeep.codes1@gmail.com>
2025-12-23 16:33:25 +0530

    add login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,86 @@
+import Container from "@/components/Container/Container";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
+  return (
+    <Container>
+      <div className="flex justify-center mt-5">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-center font-bold text-xl">
+              Login
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                    defaultValue="test"
+                    {...register("example")}
+                  />
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    {...register("exampleRequired", { required: true })}
+                  />
+                  {errors.exampleRequired && (
+                    <span>This field is required</span>
+                  )}
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </Container>
+  );
+}
