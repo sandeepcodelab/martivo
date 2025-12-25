87308a6ee029c0af7ac41de5da6a1af7b88a8fa9
sandeepcodelab <sandeep.codes1@gmail.com>
2025-12-25 21:10:29 +0530

    added user and admin both layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,67 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import UserLayout from "./layout/UserLayout";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import ProductDetails from "./pages/ProductDetails";
+import Login from "./pages/Login";
+import Checkout from "./pages/Checkout";
 
-createRoot(document.getElementById('root')).render(
+// Admin
+import Dashboard from "./admin/pages/Dashboard";
+import AdminLayout from "./layout/AdminLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    Component: UserLayout,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/product-details",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "/Login",
+        element: <Login />,
+      },
+    ],
+  },
+
+  // Admin Routes
+  {
+    path: "/admin",
+    Component: AdminLayout,
+    children: [
+      {
+        path: "",
+        element: "",
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
