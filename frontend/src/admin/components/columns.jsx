3889a2f229b110ccc963ed7df3698b1d4cec42f3
sandeepcodelab <sandeep.codes1@gmail.com>
2025-12-29 21:54:25 +0530

    added columns for tabel

---
+++ frontend/src/admin/components/columns.jsx
@@ -0,0 +1,14 @@
+export const userColumns = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+];
