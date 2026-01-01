b1106dca6f128dbc7caa2ff5e4219797131ca7f1
sandeepcodelab <sandeep.codes1@gmail.com>
2026-01-01 22:39:02 +0530

    added new columns for order table

--- frontend/src/admin/components/columns.jsx
+++ frontend/src/admin/components/columns.jsx
@@ -1,3 +1,4 @@
+// User columns
 export const userColumns = [
   {
     accessorKey: "name",
@@ -10,5 +11,37 @@ export const userColumns = [
   {
     accessorKey: "role",
     header: "Role",
+  },
+];
+
+// Order columns
+export const OrderColumns = [
+  {
+    accessorKey: "orderId",
+    header: "Order ID",
+  },
+  {
+    accessorKey: "customer",
+    header: "Customer",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "totalAmount",
+    header: "Total Amount",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Payment Method",
+  },
+  {
+    accessorKey: "paymentStatus",
+    header: "Payment Status",
+  },
+  {
+    accessorKey: "orderDate",
+    header: "Order Date",
   },
 ];
