15465fe812ca01cd1f3643d7809308944c5744f1
sandeepcodelab <sandeep.codes1@gmail.com>
2026-01-08 21:17:22 +0530

    added form

--- frontend/src/admin/pages/AddProduct.jsx
+++ frontend/src/admin/pages/AddProduct.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useRef, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -15,7 +15,6 @@ import {
   Card,
   CardAction,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -27,120 +26,189 @@ import ReactQuill from "react-quill-new";
 import "react-quill-new/dist/quill.snow.css";
 
 export default function AddProduct() {
-  const [content, setContent] = useState("");
+  const uploadThumbnail = useRef(null);
 
-  //   const {
-  //     register,
-  //     handleSubmit,
-  //     watch,
-  //     formState: { errors },
-  //   } = useForm();
+  const handelUploadThumbnail = () => {
+    uploadThumbnail.current?.click();
+  };
 
-  //   const onSubmit = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  //   console.log(watch("example")); // watch input value by passing the name of it
+  const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="w-full flex gap-10 items-start">
-      <div className="border-1 rounded-2xl w-4/6 p-5 bg-card text-card-foreground">
-        <Input type="text" placeholder="Title" className="mb-5" />
-
-        <Select>
-          <SelectTrigger className="w-full mb-5">
-            <SelectValue placeholder="Select a fruit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Fruits</SelectLabel>
-              <SelectItem value="apple">Apple</SelectItem>
-              <SelectItem value="banana">Banana</SelectItem>
-              <SelectItem value="blueberry">Blueberry</SelectItem>
-              <SelectItem value="grapes">Grapes</SelectItem>
-              <SelectItem value="pineapple">Pineapple</SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-
-        {/* Text Editor */}
-        <div className="w-full">
-          <ReactQuill
-            value={content}
-            onChange={setContent}
-            theme="snow"
-            placeholder="Start typing..."
-          />
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-between mb-5">
+          <div className="text-2xl font-bold pl-4">Add new product</div>
+          <div>
+            <Button type="submit" className="text-white cursor-pointer">
+              Save
+            </Button>
+          </div>
         </div>
-      </div>
-
-      <div className="rounded-2xl w-2/6">
-        {/* Product Image (thumbnail) */}
-        <Card className="mb-5">
-          <CardHeader>
-            <CardTitle>Product Image</CardTitle>
-            {/* <CardDescription>Card Description</CardDescription> */}
-            <CardAction>
-              <Trash2
-                size={14}
-                className="text-white hover:text-red-500 cursor-pointer"
+        <div className="w-full flex gap-10 items-start">
+          <div className="border-1 rounded-2xl w-4/6 p-5 bg-card text-card-foreground">
+            <div>
+              <Input
+                type="text"
+                placeholder="Title"
+                {...register("title", { required: true })}
+                className="mb-5"
               />
-            </CardAction>
-          </CardHeader>
-          <CardContent className="h-50">
-            <img
-              src="http://www.listercarterhomes.com/wp-content/uploads/2013/11/dummy-image-square.jpg"
-              className="w-full h-full rounded-2xl"
-            />
-          </CardContent>
-          <CardFooter>
-            <Button className="bg-primary text-white w-full">
-              <Upload />
-              Upload Image
-            </Button>
-          </CardFooter>
-        </Card>
-
-        {/* Product Images  */}
-        <Card>
-          <CardHeader>
-            <CardTitle>Product Images</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid grid-cols-3 gap-3">
-              {[1, 2, 3, 4].map((_, index) => (
-                <div key={index} className="w-20 h-auto relative">
-                  <img
-                    src="http://www.listercarterhomes.com/wp-content/uploads/2013/11/dummy-image-square.jpg"
-                    className="w-full h-full rounded-2xl"
-                  />
-
-                  <CircleX
-                    strokeWidth={3}
-                    size={20}
-                    className="absolute -top-1 -right-1 text-red-500 bg-red-200 rounded-full cursor-pointer"
-                  />
-                </div>
-              ))}
+              {errors.title && <span>This field is required</span>}
             </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="bg-primary text-white w-full">
-              <Upload />
-              Upload Images
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
 
-      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      {/* <form onSubmit={handleSubmit(onSubmit)}> */}
-      {/* register your input into the hook by invoking the "register" function */}
-      {/* <input defaultValue="test" {...register("example")} /> */}
-      {/* include validation with required or other standard HTML validation rules */}
-      {/* <input {...register("exampleRequired", { required: true })} /> */}
-      {/* errors will return when field validation fails  */}
-      {/* {errors.exampleRequired && <span>This field is required</span>} */}
-      {/* <input type="submit" /> */}
-      {/* </form> */}
+            <div>
+              <Controller
+                name="category"
+                control={control}
+                rules={{ required: "Description is required" }}
+                render={({ field }) => (
+                  <Select
+                    value={field.value ?? ""}
+                    onValueChange={field.onChange}
+                  >
+                    <SelectTrigger className="w-full mb-5">
+                      <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Categories</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                )}
+              />
+              {errors.category && <span>This field is required</span>}
+            </div>
+
+            {/* Text Editor */}
+            <div className="w-full">
+              <Controller
+                name="description"
+                control={control}
+                rules={{
+                  validate: (value) => {
+                    const text = value.replace(/<[^>]*>/g, "").trim();
+                    return (
+                      text.length >= 10 || "Minimum 10 characters required"
+                    );
+                  },
+                }}
+                render={({ field }) => (
+                  <ReactQuill
+                    theme="snow"
+                    placeholder="Description..."
+                    value={field.value || ""}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+              {errors.description?.message && (
+                <span>This field is required</span>
+              )}
+            </div>
+          </div>
+
+          <div className="rounded-2xl w-2/6">
+            {/* Product Image (thumbnail) */}
+            <Card className="mb-5">
+              <CardHeader>
+                <CardTitle>Product Image</CardTitle>
+                <CardAction>
+                  <Trash2
+                    size={14}
+                    className="text-white hover:text-red-500 cursor-pointer"
+                  />
+                </CardAction>
+              </CardHeader>
+              <CardContent className="w-full h-[250px]">
+                <img
+                  src="http://www.listercarterhomes.com/wp-content/uploads/2013/11/dummy-image-square.jpg"
+                  className="w-full h-full rounded-2xl object-cover"
+                />
+              </CardContent>
+              <CardFooter>
+                {/* <Input
+                  type="file"
+                  ref={uploadThumbnail}
+                  {...register("thumbnail")}
+                  className="hidden"
+                /> */}
+                <Input
+                  type="file"
+                  className="hidden"
+                  {...register("thumbnail", { required: true })}
+                  ref={(el) => {
+                    register("thumbnail").ref(el);
+                    uploadThumbnail.current = el;
+                  }}
+                />
+
+                <Button
+                  type="button"
+                  onClick={handelUploadThumbnail}
+                  className="text-white w-full cursor-pointer"
+                >
+                  <Upload />
+                  Upload Image
+                </Button>
+              </CardFooter>
+              {errors.thumbnail && <span>This field is required</span>}
+            </Card>
+
+            {/* Product Images  */}
+            <Card>
+              <CardHeader>
+                <CardTitle>Product Images</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
+                  {[1, 2, 3, 4, 5].map((_, index) => (
+                    <div key={index} className="relative aspect-square">
+                      <img
+                        src="http://www.listercarterhomes.com/wp-content/uploads/2013/11/dummy-image-square.jpg"
+                        className="w-full h-full rounded-2xl"
+                      />
+
+                      <CircleX
+                        strokeWidth={3}
+                        size={20}
+                        className="absolute -top-1 -right-1 text-red-500 bg-red-200 rounded-full cursor-pointer"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button
+                  type="button"
+                  className="text-white w-full cursor-pointer"
+                >
+                  <Upload />
+                  Upload Images
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+
+          {/* errors will return when field validation fails  */}
+
+          {/* <input type="submit" /> */}
+        </div>
+      </form>
     </div>
   );
 }
