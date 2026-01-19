340ef976be1aada47b6893a8f51ca2a3f745c007
sandeepcodelab <sandeep.codes1@gmail.com>
2026-01-19 16:42:20 +0530

    modified user and admin routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { ThemeProvider } from "@/components/Providers/ThemeProvider";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-// import App from "./App";
+// User routes
 import UserLayout from "./layout/UserLayout";
-import { RouterProvider, createBrowserRouter } from "react-router";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
@@ -13,7 +13,7 @@ import ProductDetails from "./pages/ProductDetails";
 import Login from "./pages/Login";
 import Checkout from "./pages/Checkout";
 
-// Admin
+// Admin routes
 import AdminLayout from "./layout/AdminLayout";
 import Dashboard from "./admin/pages/Dashboard";
 import ProductsTable from "./admin/pages/Products";
@@ -21,68 +21,34 @@ import Orders from "./admin/pages/Orders";
 import Users from "./admin/pages/Users";
 import Categories from "./admin/pages/Categories";
 import AddProduct from "./admin/pages/AddProduct";
+import EditProduct from "./admin/pages/EditProduct";
 
 const router = createBrowserRouter([
   {
-    path: "",
-    Component: UserLayout,
+    path: "/",
+    element: <UserLayout />,
     children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/products",
-        element: <Products />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/product-details",
-        element: <ProductDetails />,
-      },
-      {
-        path: "/checkout",
-        element: <Checkout />,
-      },
-      {
-        path: "/Login",
-        element: <Login />,
-      },
+      { index: true, element: <HomePage /> },
+      { path: "products", element: <Products /> },
+      { path: "cart", element: <Cart /> },
+      { path: "product-details", element: <ProductDetails /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "Login", element: <Login /> },
     ],
   },
 
   // Admin Routes
   {
     path: "/admin",
-    Component: AdminLayout,
+    element: <AdminLayout />,
     children: [
-      {
-        path: "/admin",
-        element: <Dashboard />,
-      },
-      {
-        path: "/admin/products",
-        element: <ProductsTable />,
-      },
-      {
-        path: "/admin/products/addProduct",
-        element: <AddProduct />,
-      },
-      {
-        path: "/admin/orders",
-        element: <Orders />,
-      },
-      {
-        path: "/admin/categories",
-        element: <Categories />,
-      },
-      {
-        path: "/admin/users",
-        element: <Users />,
-      },
+      { index: true, element: <Dashboard /> },
+      { path: "products", element: <ProductsTable /> },
+      { path: "products/add", element: <AddProduct /> },
+      { path: "products/edit", element: <EditProduct /> },
+      { path: "orders", element: <Orders /> },
+      { path: "categories", element: <Categories /> },
+      { path: "users", element: <Users /> },
     ],
   },
 ]);
