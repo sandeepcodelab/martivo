d775f64ede2d501920cd7b1ff77e42bbd633f907
sandeepcodelab <sandeep.codes1@gmail.com>
2026-01-22 23:54:43 +0530

    added context api for user auth

---
+++ frontend/src/contexts/AuthContextProvider.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+
+const AuthContaxtProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  // Login
+  const userLogin = (userData) => {
+    setUser(userData?.data?.user);
+    localStorage.setItem("userAuth", JSON.stringify(userData));
+  };
+
+  // Logout
+  const userLogout = () => {
+    setUser(null);
+    localStorage.removeItem("userAuth");
+  };
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("userAuth"));
+    if (storedUser) {
+      setUser(storedUser?.data?.user);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, userLogin, userLogout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContaxtProvider;
