a1caae68665fa8e4d8e1e34916df477e58ddd5c1
sandeepcodelab <sandeep.codes1@gmail.com>
2026-01-28 22:43:39 +0530

    add components

---
+++ frontend/src/components/AdminSidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import {
+  Store,
+  Package,
+  ShoppingBag,
+  LayoutGrid,
+  UserRound,
+  Gauge,
+} from "lucide-react";
+
+import { MainMenu } from "./MainMenu";
+import { SidebarUser } from "./SidebarUser";
+import { SidebarHeaderTitle } from "./SidebarHeaderTitle";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+
+// Sample data
+const data = {
+  headerData: {
+    name: "Martivo",
+    logo: Store,
+  },
+  dashboard: {
+    name: "Dashboard",
+    url: "/admin",
+    icon: Gauge,
+  },
+  dropdowns: [
+    {
+      title: "Products",
+      url: "#",
+      icon: Package,
+      isActive: true,
+      items: [
+        { title: "All Products", url: "/admin/products" },
+        { title: "Add Product", url: "/admin/products/add" },
+      ],
+    },
+  ],
+  menus: [
+    {
+      name: "Order-Details",
+      url: "/admin/orders/order-details",
+      icon: ShoppingBag,
+    },
+    { name: "Orders", url: "/admin/orders", icon: ShoppingBag },
+    { name: "Categories", url: "/admin/categories", icon: LayoutGrid },
+    { name: "Users", url: "/admin/users", icon: UserRound },
+  ],
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+};
+
+export function AdminSidebar(props) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarHeaderTitle headerData={data.headerData} />
+      </SidebarHeader>
+
+      <SidebarContent>
+        <MainMenu
+          dashboard={data.dashboard}
+          dropdowns={data.dropdowns}
+          menus={data.menus}
+        />
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarUser user={data.user} />
+      </SidebarFooter>
+
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ frontend/src/components/DataTable/Data-Table.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  getPaginationRowModel,
+  //   getFilteredRowModel,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import { DataTablePagination } from "../Pagination/Pagination";
+
+export function DataTable({ columns, data }) {
+  const [sorting, setSorting] = useState([]);
+  const [columnFilters, setColumnFilters] = useState([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    // getSortedRowModel: getSortedRowModel(),
+    // getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+    },
+  });
+
+  return (
+    <div>
+      {/* Table */}
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader className="dark:bg-neutral-800 bg-neutral-300 pointer-events-none">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      {/* Pagination controls */}
+      <div className="pt-4">
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  );
+}
