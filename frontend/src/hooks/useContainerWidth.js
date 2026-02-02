72ae221be1e3ef1a5828689dc1a0d515dd0b0767
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-02 18:36:32 +0530

    removed addition code

--- frontend/src/hooks/useContainerWidth.js
+++ frontend/src/hooks/useContainerWidth.js
@@ -17,28 +17,3 @@ export function useContainerWidth(ref) {
 
   return width;
 }
-
-// import { useLayoutEffect, useState } from "react";
-
-// export function useContainerWidth(ref) {
-//   const [width, setWidth] = useState(0);
-
-//   useLayoutEffect(() => {
-//     if (!ref.current) return;
-
-//     const element = ref.current;
-
-//     // Set initial width immediately
-//     setWidth(element.getBoundingClientRect().width);
-
-//     const observer = new ResizeObserver(([entry]) => {
-//       setWidth(entry.contentRect.width);
-//     });
-
-//     observer.observe(element);
-
-//     return () => observer.disconnect();
-//   }, []);
-
-//   return width;
-// }
