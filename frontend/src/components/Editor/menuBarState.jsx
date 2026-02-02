3738621cdd87e5fdfd1af6ed1f39c8f1b3de4ac3
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-02 18:44:08 +0530

    added can state for some buttons

--- frontend/src/components/Editor/menuBarState.jsx
+++ frontend/src/components/Editor/menuBarState.jsx
@@ -21,10 +21,12 @@ export function menuBarStateSelector(ctx) {
     canCode: ctx.editor.can().chain().toggleCode().run() || false,
 
     canClearMarks: ctx.editor.can().chain().unsetAllMarks().run() || false,
+    canClearNodes: ctx.editor.can().chain().clearNodes().run() || false,
 
     // Block types
     isParagraph: ctx.editor.isActive("paragraph") || false,
 
+    // Heading active state
     isHeading1: ctx.editor.isActive("heading", { level: 1 }) || false,
     isHeading2: ctx.editor.isActive("heading", { level: 2 }) || false,
     isHeading3: ctx.editor.isActive("heading", { level: 3 }) || false,
@@ -32,20 +34,62 @@ export function menuBarStateSelector(ctx) {
     isHeading5: ctx.editor.isActive("heading", { level: 5 }) || false,
     isHeading6: ctx.editor.isActive("heading", { level: 6 }) || false,
 
-    // Lists and blocks
+    // Heading can state
+    canHeading1:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 1 }).run() ||
+      false,
+    canHeading2:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 2 }).run() ||
+      false,
+    canHeading3:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 3 }).run() ||
+      false,
+    canHeading4:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 4 }).run() ||
+      false,
+    canHeading5:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 5 }).run() ||
+      false,
+    canHeading6:
+      ctx.editor.can().chain().focus().toggleHeading({ level: 6 }).run() ||
+      false,
+
+    // Lists and blocks active state
     isBulletList: ctx.editor.isActive("bulletList") || false,
     isOrderedList: ctx.editor.isActive("orderedList") || false,
     isCodeBlock: ctx.editor.isActive("codeBlock") || false,
     isBlockquote: ctx.editor.isActive("blockquote") || false,
 
-    // Text alignment
+    // Lists and blocks can state
+    canBulletList:
+      ctx.editor.can().chain().focus().toggleBulletList().run() || false,
+    canOrderedList:
+      ctx.editor.can().chain().focus().toggleOrderedList().run() || false,
+    canBlockquote:
+      ctx.editor.can().chain().focus().toggleBlockquote().run() || false,
+
+    // Text alignment active state
     isAlignLeft: ctx.editor.isActive({ textAlign: "left" }) || false,
     isAlignCenter: ctx.editor.isActive({ textAlign: "center" }) || false,
     isAlignRight: ctx.editor.isActive({ textAlign: "right" }) || false,
     isAlignJustify: ctx.editor.isActive({ textAlign: "justify" }) || false,
 
+    // Text alignment active state
+    canAlignLeft:
+      ctx.editor.can().chain().focus().setTextAlign("left").run() || false,
+    canAlignCenter:
+      ctx.editor.can().chain().focus().setTextAlign("center").run() || false,
+    canAlignRight:
+      ctx.editor.can().chain().focus().setTextAlign("right").run() || false,
+    canAlignJustify:
+      ctx.editor.can().chain().focus().setTextAlign("justify").run() || false,
+
     // History
     canUndo: ctx.editor.can().chain().undo().run() || false,
     canRedo: ctx.editor.can().chain().redo().run() || false,
+
+    // LineBreak
+    canHardBreak:
+      ctx.editor.can().chain().focus().setHardBreak().run() || false,
   };
 }
