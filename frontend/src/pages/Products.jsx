a6054f78e7f2d2c4035cfc230e4c4bc403b2ca10
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-03 22:08:40 +0530

    add product api

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,38 +1,66 @@
 import Container from "@/components/Container/Container";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { Heart } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/v1/product/all")
+      .then((res) => {
+        setProducts(res.data?.data.products);
+        return;
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  // console.log(products);
+
   return (
     <section className="py-2 text-white">
       <Container>
         <h2 className="text-3xl font-bold mb-8">Best Sellers</h2>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {Array.from({ length: 9 }).map((item) => (
+          {products.map((product) => (
             <Card
-              key={item}
+              key={product?.title}
               className="hover:shadow-md transition py-0 pb-6 gap-1"
             >
-              <div className="w-full h-50 rounded-t-lg overflow-hidden relative">
-                <img
-                  src="https://placehold.co/400x400/gray/white"
-                  alt=""
-                  className="w-full h-full object-cover"
-                />
-                <div className="absolute top-3 right-3 bg-white rounded-full p-1">
-                  <Heart className="size-5 fill-gray-300 text-gray-300" />
+              <CardContent className="px-0">
+                <div className="w-full h-60 rounded-t-lg overflow-hidden relative border-b">
+                  <Link to={`/product-details/${product?._id}`}>
+                    <img
+                      src={product?.thumbnail.url}
+                      alt=""
+                      className="w-full h-full aspect-square"
+                    />
+                  </Link>
+                  <div className="absolute top-2 right-2">
+                    <Heart className="size-6 fill-gray-400" />
+                  </div>
                 </div>
-              </div>
-              <CardHeader className="mt-3">
-                <CardTitle>Product {item}</CardTitle>
-              </CardHeader>
-              <CardContent>
+              </CardContent>
+              <CardFooter className="grid px-4">
+                <Link to={`/product-details/${product?._id}`}>
+                  <p className="text-lg py-1">{product?.title}</p>
+                </Link>
                 <p className="text-muted-foreground mb-2">$99.00</p>
-                <Button size="sm" className="w-full cursor-pointer">
+
+                <Button size="sm" className="w-full text-white cursor-pointer">
                   Add to Cart
                 </Button>
-              </CardContent>
+              </CardFooter>
             </Card>
           ))}
         </div>
