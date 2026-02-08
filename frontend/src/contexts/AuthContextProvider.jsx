ec36b5e3df23cf29f1174e67445f92e4c144635c
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-08 14:35:34 +0530

    added functionality globle to count cart items

--- frontend/src/contexts/AuthContextProvider.jsx
+++ frontend/src/contexts/AuthContextProvider.jsx
@@ -1,8 +1,36 @@
 import { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
+import axios from "axios";
+import { notification } from "@/utils/toast";
+// import { useNavigate } from "react-router";
 
 const AuthContaxtProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const [itemsCount, setItemCount] = useState();
+
+  // const navigate = useNavigate();
+
+  // Refresh auth on app load
+  // useEffect(() => {
+  //   axios
+  //     .post("/api/v1/auth/refresh", {}, { withCredentials: true })
+  //     .then((res) => console.log("res => ", res))
+  //     .catch((err) => {
+  //       if (err.status === 401) {
+  //         // userLogout();
+  //       }
+  //     });
+
+  //   const storedUser = JSON.parse(localStorage.getItem("userAuth"));
+  //   if (storedUser) {
+  //     setUser(storedUser?.data?.user);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    const cartItems = JSON.parse(localStorage.getItem("guestCartItems")) || [];
+    setItemCount(cartItems.length);
+  }, []);
 
   // Login
   const userLogin = (userData) => {
@@ -12,19 +40,33 @@ const AuthContaxtProvider = ({ children }) => {
 
   // Logout
   const userLogout = () => {
+    axios
+      .post("/api/v1/auth/logout", {}, { withCredentials: true })
+      .then((res) => {
+        // userLogout();
+
+        notification.success("Log out successfully.");
+        navigate("/auth/login");
+      })
+      .catch((error) => {
+        console.log(error);
+        notification.error("Something went wrong, Please try again later.");
+      });
+
     setUser({});
     localStorage.removeItem("userAuth");
   };
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("userAuth"));
-    if (storedUser) {
-      setUser(storedUser?.data?.user);
-    }
-  }, []);
+  // Count cart items
+  const updateCartCount = () => {
+    const cartItems = JSON.parse(localStorage.getItem("guestCartItems")) || [];
+    setItemCount(cartItems.length);
+  };
 
   return (
-    <AuthContext.Provider value={{ user, userLogin, userLogout }}>
+    <AuthContext.Provider
+      value={{ user, userLogin, userLogout, itemsCount, updateCartCount }}
+    >
       {children}
     </AuthContext.Provider>
   );
