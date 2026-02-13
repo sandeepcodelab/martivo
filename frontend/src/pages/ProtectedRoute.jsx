66c95786b561380a486574caa4cdb800852814c8
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-13 18:59:50 +0530

    check routes

---
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import AuthContext from "@/contexts/AuthContext";
+import { useContext, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+
+export default function ProtectedRoute({ children, authentication = true }) {
+  const { userData } = useContext(AuthContext);
+  const isAuthenticated = userData.isAuthenticated || false;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (authentication && authentication !== isAuthenticated) {
+      navigate("/auth/login", { state: { from: location }, replace: true });
+    } else if (!authentication && authentication !== isAuthenticated) {
+      navigate(location?.state?.from?.pathname || "/", { replace: true });
+    }
+  }, [isAuthenticated, authentication, navigate]);
+
+  return children;
+}
