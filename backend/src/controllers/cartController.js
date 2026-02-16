f9bd37f89d3d05ac2901839784f369174992c662
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-16 16:28:42 +0530

    added cart merge functionality and updated get cart items

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -2,12 +2,16 @@ import { Cart } from "../models/cartModel.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
+import { Variant } from "../models/variantModel.js";
 import crypto from "crypto";
 
 const addItem = asyncHandler(async (req, res) => {
   const { variantId, quantity } = req.body;
   const userId = req?.user?._id;
-  const guestToken = localStorage.getItem("guestID");
+  console.table([variantId, quantity, userId]);
+  return;
+  // const guestToken = localStorage.getItem("guestID");
+  const guestToken = "";
 
   if (!variantId) {
     throw new ApiError(
@@ -19,63 +23,63 @@ const addItem = asyncHandler(async (req, res) => {
   const expiryAt = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000);
   let hashedToken = "";
 
-  if (!userId) {
-    if (!guestToken) {
-      const unHashedToken = crypto.randomBytes(32).toString("hex");
-      hashedToken = crypto
-        .createHash("sha256")
-        .update(unHashedToken)
-        .digest("hex");
+  // if (!userId) {
+  //   if (!guestToken) {
+  //     const unHashedToken = crypto.randomBytes(32).toString("hex");
+  //     hashedToken = crypto
+  //       .createHash("sha256")
+  //       .update(unHashedToken)
+  //       .digest("hex");
 
-      localStorage.setItem("guestID", unHashedToken);
-    }
+  //     localStorage.setItem("guestID", unHashedToken);
+  //   }
 
-    const guestId = crypto
-      .createHash("sha256")
-      .update(guestToken)
-      .digest("hex");
+  //   const guestId = crypto
+  //     .createHash("sha256")
+  //     .update(guestToken)
+  //     .digest("hex");
 
-    const guestCart = await Cart.findOne({ guestId });
+  //   const guestCart = await Cart.findOne({ guestId });
 
-    if (!guestCart) {
-      const item = await Cart.create({
-        guestId: hashedToken,
-        items: [{ variantId, quantity }],
-        expiryAt,
-      });
+  //   if (!guestCart) {
+  //     const item = await Cart.create({
+  //       guestId: hashedToken,
+  //       items: [{ variantId, quantity }],
+  //       expiryAt,
+  //     });
 
-      if (!item) {
-        throw new ApiError(
-          500,
-          "Unable to add item to cart. Please try again later.",
-          []
-        );
-      }
+  //     if (!item) {
+  //       throw new ApiError(
+  //         500,
+  //         "Unable to add item to cart. Please try again later.",
+  //         []
+  //       );
+  //     }
 
-      return res
-        .status(201)
-        .json(new ApiResponse(201, { item }, "Item added to cart."));
-    }
+  //     return res
+  //       .status(201)
+  //       .json(new ApiResponse(201, { item }, "Item added to cart."));
+  //   }
 
-    const gustCartItems = guestCart.items;
-    gustCartItems.push({ variantId, quantity });
+  //   const gustCartItems = guestCart.items;
+  //   gustCartItems.push({ variantId, quantity });
 
-    guestCart.items = gustCartItems;
-    guestCart.expiryAt = expiryAt;
-    await guestCart.save({ validateBeforeSave: false });
+  //   guestCart.items = gustCartItems;
+  //   guestCart.expiryAt = expiryAt;
+  //   await guestCart.save({ validateBeforeSave: false });
 
-    if (!guestCart) {
-      throw new ApiError(
-        500,
-        "Unable to add item to cart. Please try again later.",
-        []
-      );
-    }
+  //   if (!guestCart) {
+  //     throw new ApiError(
+  //       500,
+  //       "Unable to add item to cart. Please try again later.",
+  //       []
+  //     );
+  //   }
 
-    return res
-      .status(201)
-      .json(new ApiResponse(201, { item: existedCart }, "Item added to cart."));
-  }
+  //   return res
+  //     .status(201)
+  //     .json(new ApiResponse(201, { item: existedCart }, "Item added to cart."));
+  // }
 
   const userCart = await Cart.findOne({ userId });
 
@@ -117,7 +121,8 @@ const addItem = asyncHandler(async (req, res) => {
 });
 
 const getCart = asyncHandler(async (req, res) => {
-  const cart = await Cart.find();
+  const userId = req?.user?._id;
+  const cart = await Cart.findOne({ userId });
 
   if (!cart) {
     return res
@@ -181,4 +186,60 @@ const deleteCartItem = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Item deleted successfully."));
 });
 
-export { addItem, getCart, updateCart, deleteCartItem };
+const mergeCart = asyncHandler(async (req, res) => {
+  const { items = [] } = req.body || {};
+  const userId = req.user._id;
+
+  if (!items.length) return;
+
+  const cart = await Cart.findOne({ userId });
+
+  if (!cart) {
+    const createCart = await Cart.create({
+      items,
+      userId,
+    });
+
+    if (!createCart) {
+      throw new ApiError(500, "Unable to add items to cart.");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { cart: createCart },
+          "Item added to cart successfully."
+        )
+      );
+  }
+
+  for (const localItem of items) {
+    const variant = await Variant.findById(localItem.variantId);
+
+    const existingItem = cart.items.find(
+      (cartItem) =>
+        cartItem.variantId.toString() === localItem.variantId.toString()
+    );
+
+    if (existingItem) {
+      const totalQTY = (existingItem.quantity += localItem.quantity);
+
+      if (totalQTY > variant.stock) {
+        existingItem.quantity = variant.stock;
+      }
+      existingItem.quantity = variant.stock;
+    } else {
+      cart.items.push(localItem);
+    }
+  }
+
+  await cart.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { cart }, "Cart merged successfully."));
+});
+
+export { addItem, getCart, updateCart, deleteCartItem, mergeCart };
