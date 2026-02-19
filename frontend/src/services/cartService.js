dd82369666e4c9f4369a47fe05056ef867498d68
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-19 19:52:36 +0530

    added mothods for cart

---
+++ frontend/src/services/cartService.js
@@ -0,0 +1,93 @@
+import api from "../utils/api";
+
+export const getCartWithVariants = async () => {
+  // Get cart
+  const cartRes = await api.get("/cart/all");
+  const cartItems = cartRes?.data?.data?.cart?.items || [];
+
+  if (!cartItems.length) return [];
+
+  const variantIds = cartItems.map((item) => item.variantId);
+
+  // Get variant details
+  const variantRes = await api.post("/v1/product-variant/bulk", {
+    variantIds,
+  });
+
+  const variants = variantRes.data.data.variants;
+
+  // Merge
+  const mergedCart = variants.map((variant) => {
+    const matchedItem = cartItems.find(
+      (item) => item.variantId === variant._id,
+    );
+
+    return {
+      ...variant,
+      quantity: matchedItem?.quantity ?? 1,
+    };
+  });
+
+  return mergedCart;
+};
+
+export const getGuestCartWithVariants = async () => {
+  const localCart = JSON.parse(localStorage.getItem("guestCartItems")) || [];
+
+  if (!localCart.length) return [];
+
+  const variantIds = localCart.map((item) => item.variantId);
+
+  const res = await api.post("/product-variant/bulk", {
+    variantIds,
+  });
+
+  const variants = res.data.data.variants;
+
+  const mergedCart = variants.map((variant) => {
+    const matchedItem = localCart.find(
+      (item) => item.variantId === variant._id,
+    );
+
+    return {
+      ...variant,
+      quantity: matchedItem?.quantity ?? 1,
+    };
+  });
+
+  return mergedCart;
+};
+
+export const addItemToCart = (variantId, quantity) => {
+  return api.post("/cart/add", { variantId, quantity });
+};
+
+export const addItemToGuestCart = (variantId, quantity) => {
+  const cart = JSON.parse(localStorage.getItem("guestCartItems")) || [];
+
+  const itemExist = cart.find((item) => item.variantId === variantId);
+
+  let updateCart;
+
+  if (itemExist) {
+    updateCart = cart.map((item) =>
+      item.variantId === variantId ? { ...item, quantity: quantity } : item,
+    );
+  } else {
+    updateCart = [...cart, { variantId, quantity }];
+  }
+
+  localStorage.setItem("guestCartItems", JSON.stringify(updateCart));
+
+  return updateCart;
+};
+
+export const updateCartAPI = (variantId, newQty) => {
+  return api.patch(`/cart/update/${variantId}`, {
+    quantity: newQty,
+  });
+};
+
+export const removeItemFromCart = (variantId) => {
+  return api.delete(`/cart/delete/${variantId}`);
+};
