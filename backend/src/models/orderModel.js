ce26e494716a08a048589a85f8a799d37d05a81f
sandeepcodelab <sandeep.codes1@gmail.com>
2026-02-27 21:56:11 +0530

    modified structure of schema

--- backend/src/models/orderModel.js
+++ backend/src/models/orderModel.js
@@ -4,25 +4,52 @@ const orderSchema = new Schema(
   {
     userId: {
       type: Schema.Types.ObjectId,
-      ref: "user",
+      ref: "User",
       required: true,
     },
-    items: [
+    orderItems: [
       {
-        type: String,
+        variant: {
+          type: Schema.Types.ObjectId,
+          ref: "Variant",
+          required: true,
+        },
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        priceAtPurchase: {
+          type: Number,
+          required: true,
+        },
       },
     ],
-    totalAmount: {
-      type: number,
-    },
-    status: {
-      type: String,
-    },
-    payment: {
-      type: String,
+    totalPrice: {
+      type: Number,
+      required: true,
     },
     shippingAddress: {
+      fullName: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
       type: String,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
+      default: "Pending",
     },
   },
   { timestamps: true }
