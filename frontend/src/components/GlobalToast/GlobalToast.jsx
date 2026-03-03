3ca15e6bdfda4b65cb1ea65f2522b0718a4dcb99
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-03 15:41:09 +0530

    added toast globaly

---
+++ frontend/src/components/GlobalToast/GlobalToast.jsx
@@ -0,0 +1,22 @@
+import { ToastContainer, Bounce } from "react-toastify";
+import { useTheme } from "../Providers/ThemeProvider";
+// import { useTheme } from "next-themes";
+
+export default function GlobalToast() {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <ToastContainer
+      position="bottom-right"
+      autoClose={4000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      theme={resolvedTheme === "dark" ? "dark" : "light"}
+      transition={Bounce}
+      pauseOnHover
+      pauseOnFocusLoss={false}
+    />
+  );
+}
