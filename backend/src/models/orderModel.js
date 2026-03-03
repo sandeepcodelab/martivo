afc57bef9b017836926c797b89e6b6fc0135ead1
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-03 15:46:02 +0530

    modified order schema

--- backend/src/models/orderModel.js
+++ backend/src/models/orderModel.js
@@ -35,16 +35,21 @@ const orderSchema = new Schema(
     },
     shippingAddress: {
       fullName: { type: String, required: true },
-      email: { type: String, required: true },
       phone: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
+      state: { type: String, required: true },
       postalCode: { type: String, required: true },
-      country: { type: String, required: true },
     },
     paymentMethod: {
       type: String,
+      enum: ["Card", "COD"],
       required: true,
+    },
+    paymentResult: {
+      paymentId: String,
+      signature: String,
+      status: String,
     },
     orderStatus: {
       type: String,
