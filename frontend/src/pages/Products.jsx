40d05bce349f25087e96b721f1b2ff92d04296e5
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-06 18:11:58 +0530

    add productCard

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,11 +1,9 @@
 import Container from "@/components/Container/Container";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Heart } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import { getAllProducts } from "@/services/productService";
 import { notification } from "@/utils/toast";
+import ProductCard from "@/components/Product/ProductCard";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
@@ -44,37 +42,9 @@ export default function Products() {
         <h2 className="text-3xl font-bold mb-8">Best Sellers</h2>
         <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {products.map((product) => (
-            <Card
-              key={product?.title}
-              className="hover:shadow-md transition py-0 pb-6 gap-1"
-            >
-              <CardContent className="px-0">
-                <div className="w-full h-60 rounded-t-lg overflow-hidden relative border-b">
-                  <Link to={`/product-details/${product?._id}`}>
-                    <img
-                      src={product?.thumbnail.url}
-                      alt=""
-                      className="w-full h-full aspect-square"
-                    />
-                  </Link>
-                  <div className="absolute top-2 right-2">
-                    <Heart className="size-6 fill-gray-400" />
-                  </div>
-                </div>
-              </CardContent>
-              <CardFooter className="grid px-4">
-                <Link to={`/product-details/${product?._id}`}>
-                  <p className="text-lg py-1 hover:underline">
-                    {product?.title}
-                  </p>
-                </Link>
-                <p className="text-muted-foreground mb-2">$99.00</p>
-                {/* 
-                <Button size="sm" className="w-full text-white cursor-pointer">
-                  Add to Cart
-                </Button> */}
-              </CardFooter>
-            </Card>
+            <Link key={product._id} to={`/product-details/${product._id}`}>
+              <ProductCard item={product} />
+            </Link>
           ))}
         </div>
       </Container>
