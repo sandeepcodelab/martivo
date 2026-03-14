50015ac8da917fa9f0b91f1f62cd2ecc2492a1dc
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-14 18:48:34 +0530

    add API url dynamically

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,34 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/v1/",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/`,
   withCredentials: true,
 });
-
-// api.interceptors.response.use(
-//   (response) => response,
-//   async (error) => {
-//     const originalRequest = error.config;
-
-//     if (
-//       error.response?.status === 401 &&
-//       !originalRequest._retry &&
-//       !originalRequest.url.includes("/auth/refresh")
-//     ) {
-//       originalRequest._retry = true;
-
-//       try {
-//         await api.post("/auth/refresh");
-
-//         return api(originalRequest);
-//       } catch (err) {
-//         window.location.href = "/auth/login";
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   },
-// );
 
 api.interceptors.response.use(
   (response) => response,
