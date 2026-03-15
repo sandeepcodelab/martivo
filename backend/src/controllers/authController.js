8879b893c418e60fa3532e1295b1f1540e18cd36
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-15 15:24:09 +0530

    fixed logout and clear cookies

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -160,8 +160,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .clearCookie("accessToken", "", options)
-    .clearCookie("refreshToken", "", options)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "Logout successfully."));
 });
 
