570a46073213c26c92b1712e338ece6775e47b6e
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-23 17:35:36 +0530

    add daynamic data in user section

--- frontend/src/components/AdminSidebar/SidebarUser.jsx
+++ frontend/src/components/AdminSidebar/SidebarUser.jsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
+import { BadgeCheck, Bell, ChevronsUpDown, LogOut } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -25,9 +18,12 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useContext } from "react";
+import AuthContext from "@/contexts/AuthContext";
 
-export function SidebarUser({ user }) {
+export function SidebarUser() {
   const { isMobile } = useSidebar();
+  const { userData } = useContext(AuthContext);
 
   return (
     <SidebarMenu>
@@ -39,15 +35,22 @@ export function SidebarUser({ user }) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">A</AvatarFallback>
+                <AvatarImage
+                  src={userData?.user?.avatar?.url}
+                  alt={userData?.user?.name}
+                />
+                <AvatarFallback className="rounded-lg">
+                  {userData?.user?.name[0].toUpperCase()}
+                </AvatarFallback>
               </Avatar>
 
               <div className="grid flex-1 text-left text-sm leading-tight">
-                {/* <span className="truncate font-medium">{user.name}</span> */}
-                {/* <span className="truncate text-xs">{user.email}</span> */}
-                <span className="truncate font-medium">Admin</span>
-                <span className="truncate text-xs">admin@gmail.com</span>
+                <span className="truncate font-medium">
+                  {userData?.user?.name}
+                </span>
+                <span className="truncate text-xs">
+                  {userData?.user?.email}
+                </span>
               </div>
 
               <ChevronsUpDown className="ml-auto size-4" />
@@ -63,15 +66,22 @@ export function SidebarUser({ user }) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">A</AvatarFallback>
+                  <AvatarImage
+                    src={userData?.user?.avatar?.url}
+                    alt={userData?.user?.name}
+                  />
+                  <AvatarFallback className="rounded-lg">
+                    {userData?.user?.name[0].toUpperCase()}
+                  </AvatarFallback>
                 </Avatar>
 
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  {/* <span className="truncate font-medium">{user.name}</span> */}
-                  {/* <span className="truncate text-xs">{user.email}</span> */}
-                  <span className="truncate font-medium">Admin</span>
-                  <span className="truncate text-xs">admin@gmail.com</span>
+                  <span className="truncate font-medium">
+                    {userData?.user?.name}
+                  </span>
+                  <span className="truncate text-xs">
+                    {userData?.user?.email}
+                  </span>
                 </div>
               </div>
             </DropdownMenuLabel>
@@ -80,21 +90,8 @@ export function SidebarUser({ user }) {
 
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-
-            <DropdownMenuSeparator />
-
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheck />
                 Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <Bell />
