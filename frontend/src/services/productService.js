9b358009e1169d4dbaf1ff686c4291de56a1d399
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-26 18:27:53 +0530

    add services for create product and create variants

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -8,6 +8,18 @@ export const getProduct = (productId) => {
   return api.get(`/product/singleProduct/${productId}`);
 };
 
+export const createProduct = (formData) => {
+  return api.post("/product/add", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
 export const getAllVariants = (productId) => {
   return api.get(`/product-variant/${productId}/all`);
+};
+
+export const addBulkVariants = (productId, variants) => {
+  return api.post(`/product-variant/${productId}/addBulk`, { variants });
 };
