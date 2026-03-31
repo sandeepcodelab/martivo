4286d220ca3eb5514662df1f4f25027cc1cdebd8
sandeepcodelab <sandeep.codes1@gmail.com>
2026-03-31 16:45:47 +0530

    add dynamica data for products table

--- frontend/src/components/DataTable/Columns.jsx
+++ frontend/src/components/DataTable/Columns.jsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { Pencil, Trash } from "lucide-react";
+import { Pencil, SquarePen, Trash } from "lucide-react";
 import { Link } from "react-router";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 // User columns
 export const userColumns = [
@@ -63,39 +68,58 @@ export const CategoryColumns = (onEdit, onDelete) => [
   {
     accessorKey: "status",
     header: "Status",
-    cell: ({ row }) =>
-      row?.original?.status ? (
-        <span className="text-green-500">Active</span>
+    cell: ({ row }) => {
+      const status = row?.original?.status;
+      return status === "active" ? (
+        <span className="text-green-500 capitalize">{status}</span>
       ) : (
-        <span className="text-red-500">Inactive</span>
-      ),
+        <span className="text-red-500 capitalize">{status}</span>
+      );
+    },
   },
   {
     id: "action",
     header: "Action",
     cell: ({ row }) => (
-      <div className="flex gap-2">
-        <Button
-          size="icon"
-          variant="outline"
-          onClick={() => onEdit(row.original)}
-        >
-          <Pencil />
-        </Button>
-        <Button
-          size="icon"
-          variant="destructive"
-          onClick={() => onDelete(row.original)}
-        >
-          <Trash />
-        </Button>
+      <div className="flex gap-4">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => onEdit(row.original)}
+              className="cursor-pointer"
+            >
+              <SquarePen size={20} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Edit</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              size="icon"
+              variant="destructive"
+              onClick={() => onDelete(row.original)}
+              className="cursor-pointer"
+            >
+              <Trash size={20} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Delete</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
     ),
   },
 ];
 
 // Product columns
-export const ProductColumns = [
+export const ProductColumns = (onDelete) => [
   // {
   //   accessorKey: "productName",
   //   header: "Product",
@@ -110,43 +134,150 @@ export const ProductColumns = [
   //   },
   // },
   {
-    accessorKey: "productName",
+    accessorKey: "title",
     header: "Product",
     cell: ({ row }) => {
       const product = row.original;
 
       return (
         <Link
-          to={`/admin/products/edit/`}
-          className="font-medium hover:text-primary hover:underline"
+          to={`/admin/products/edit/${product._id}`}
+          className="flex items-center gap-2 font-medium hover:text-primary hover:underline cursor-pointer"
         >
-          {product.productName}
+          <div className="w-[50px] rounded overflow-hidden">
+            <img src={product?.thumbnail?.url} alt="Image" />
+          </div>
+          <p className="max-w-[300px] truncate pr-5">{product.title}</p>
         </Link>
       );
     },
   },
   {
-    accessorKey: "category",
+    accessorKey: "category.name",
     header: "Category",
   },
   {
-    accessorKey: "price",
+    accessorKey: "minPrice",
     header: "Price",
+    cell: ({ row }) => {
+      const product = row.original;
+
+      if (product?.minPrice && product?.maxPrice) {
+        return <span>{`₹${product.minPrice} (₹${product.maxPrice})`}</span>;
+      } else {
+        return <span></span>;
+      }
+    },
+  },
+  {
+    accessorKey: "variants",
+    header: "Variants",
+    cell: ({ row }) => {
+      const variants = row?.original?.variants;
+
+      return (
+        <>
+          <p>{variants.length} variants</p>
+          {variants[0]?.size ? <span>{variants[0]?.size} | </span> : ""}
+          {variants[0]?.color ? <span>{variants[0]?.color} | </span> : ""}
+          {variants[0]?.salePrice ? <span>₹{variants[0]?.salePrice}</span> : ""}
+        </>
+      );
+    },
   },
   {
     accessorKey: "stock",
     header: "Stock",
+    cell: ({ row }) => {
+      const variants = row.original.variants;
+
+      const totalStock = variants?.reduce(
+        (totalStock, variant) => totalStock + variant.stock,
+        0,
+      );
+      const outOfStock = variants?.reduce(
+        (totalStock, variant) => totalStock + (variant.stock === 0),
+        0,
+      );
+
+      return (
+        <>
+          {totalStock > 0 ? <span>{totalStock} units</span> : ""}
+          {outOfStock > 0 ? (
+            <span className="pl-1 text-red-500">
+              ({outOfStock} out of stock)
+            </span>
+          ) : (
+            ""
+          )}
+        </>
+      );
+    },
   },
   {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => {
+      const status = row.original.status;
+
+      return status === "active" ? (
+        <span className="text-green-500 capitalize">{status}</span>
+      ) : (
+        <span className="text-red-500 capitalize">{status}</span>
+      );
+    },
   },
   {
     accessorKey: "createdAt",
     header: "Created At",
+    cell: ({ row }) => {
+      const product = row.original;
+      const date = new Date(product.createdAt);
+
+      return (
+        <span>
+          {date.toLocaleDateString("en-IN", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          })}
+        </span>
+      );
+    },
   },
   {
     id: "actions",
     header: "Actions",
+    cell: ({ row }) => {
+      return (
+        <div className="flex gap-4">
+          {/* <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="outline" size="icon" className="cursor-pointer">
+                <SquarePen size={20} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Edit</p>
+            </TooltipContent>
+          </Tooltip> */}
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="destructive"
+                size="icon"
+                className="cursor-pointer"
+                onClick={() => onDelete(row.original)}
+              >
+                <Trash size={20} />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Delete</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      );
+    },
   },
 ];
