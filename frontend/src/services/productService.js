ff812eb83aa6bd4c57de35706dcdc2ec40b4f584
sandeepcodelab <sandeep.codes1@gmail.com>
2026-04-04 14:51:31 +0530

    added functionalities to update, get and delete product

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -16,10 +16,31 @@ export const createProduct = (formData) => {
   });
 };
 
+export const updateProduct = (id, formData) => {
+  return api.patch(`/product/update/${id}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const adminGetAllProducts = (params = {}) => {
+  return api.get("/product/getAll", { params });
+};
+
+export const deleteProduct = (id) => {
+  return api.delete(`/product/delete/${id}`);
+};
+
+// Variants
 export const getAllVariants = (productId) => {
   return api.get(`/product-variant/${productId}/all`);
 };
 
 export const addBulkVariants = (productId, variants) => {
   return api.post(`/product-variant/${productId}/addBulk`, { variants });
+};
+
+export const updateVariants = (productId, variants) => {
+  return api.patch(`/product-variant/update/${productId}`, { variants });
 };
