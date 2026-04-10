294e2825fe9d56c1363289fb84f286b81942b166
sandeepcodelab <sandeep.codes1@gmail.com>
2026-04-10 17:33:17 +0530

    added functionality to make table dynamic

--- frontend/src/admin/pages/Users.jsx
+++ frontend/src/admin/pages/Users.jsx
@@ -2,100 +2,74 @@ import { useEffect, useState } from "react";
 import { userColumns } from "../../components/DataTable/Columns";
 import { DataTable } from "../../components/DataTable/Data-Table";
 import { Input } from "@/components/ui/input";
+import { getAllUsers } from "@/services/userService";
+import { notification } from "@/utils/toast";
+import { useNavigate, useSearchParams } from "react-router";
 
 export default function Users() {
-  const [searchValue, setSearchValue] = useState();
-  const [data, setData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchedValue, setSearchedValue] = useState("");
+  const [allusers, setAllUsers] = useState([]);
+  const [paginationData, setPaginationData] = useState({});
+  const navigate = useNavigate();
 
+  const search = searchParams.get("search") || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = Number(searchParams.get("limit")) || 10;
+
+  // Fetch users
   useEffect(() => {
-    // Simulate API call
-    const getData = async () => {
-      const result = [
-        {
-          id: "728ed52f",
-          name: "Tester First",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester",
-          email: "m@example.com",
-          role: "user",
-        },
-        {
-          id: "728ed52f",
-          name: "Tester Last",
-          email: "m@example.com",
-          role: "user",
-        },
-      ];
-
-      setData(result);
+    const fetchUsers = async () => {
+      try {
+        const res = await getAllUsers({ search, page, limit });
+        setAllUsers(res.data.data.users);
+        setPaginationData(res.data.data.pageInfo);
+      } catch (err) {
+        notification.error(err.response.data.message);
+      }
     };
 
-    getData();
-  }, []);
+    fetchUsers();
+  }, [search, page, limit]);
+
+  // Set filter
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchParams((prev) => {
+        prev.set("search", searchedValue);
+        prev.set("page", 1);
+        prev.set("limit", 10);
+        return prev;
+      });
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [searchedValue]);
+
+  // Edit handler
+  const onEdit = (row) => {
+    const userId = row._id;
+    navigate(`/admin/users/${userId}`);
+  };
+
+  // Delete handler
+  const onDelete = (row) => {
+    console.log(row);
+  };
 
   return (
     <div className="container mx-auto">
       <div className="mb-4">
         <Input
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchedValue}
+          onChange={(e) =>
+            setSearchedValue(e.target.value.trim().toLowerCase())
+          }
           placeholder="Search..."
           className="max-w-sm"
         />
       </div>
-      <DataTable columns={userColumns} data={data} />
+      <DataTable columns={userColumns(onEdit, onDelete)} data={allusers} />
     </div>
   );
 }
